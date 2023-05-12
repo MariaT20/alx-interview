db67028ecd0d7f51b842a62434718c7006d8faef
MariaT20 <mariatendo84@gmail.com>
2023-05-12 19:21:34 +0000

    await-request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,24 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const movieID = process.argv[2];
-const movieEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+const endpoint = 'https://swapi-api.hbtn.io/api';
+const filmId = process.argv[2];
 
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
 
-request(movieEndPoint, (res, error, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const movie = JSON.parse(body);
-    const characterUrl = movie.characters;
+  let characters = JSON.parse(body).characters;
 
-    for (const movieCharacter of characterUrl) {
-      request(movieCharacter, (res, error, body) => {
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else {
-          const movieCharacter = JSON.parse(body);
-          console.log(movieCharacter.name);
+          console.log(JSON.parse(body).name);
+          resolve(body);
         }
       });
-    }
+    });
   }
 });
